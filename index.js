ea1cfb7915018ebac3cef8115cef87580e6dc0d8
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-07-29 18:14:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 class Formatter {
+<<<<<<< HEAD
   static capitalize(str) {
     var r = /\b\w/gi
 
@@ -24,4 +25,86 @@ class Formatter {
     })
     return newArray.join(' ').charAt(0).toUpperCase() + newArray.join(' ').slice(1)
   }
+=======
+  let store = { drivers: [], passengers: [], trips: []};
+let driverId = 0
+
+class Driver {
+  constructor(name){
+    this.id = ++driverId;
+    this.name = name;
+    store.drivers.push(this);
+  }
+  trips(){
+    return store.trips.filter(
+      function(trip){
+        return trip.driverId === this.id;
+      }.bind(this)
+    );
+  }
+  passengers(){
+    return this.trips().map(
+      function(trip){
+        return trip.passenger();
+      }.bind(this)
+    );
+  }
+}
+
+let passengerId = 0
+
+class Passenger{
+  constructor(name){
+    this.id = ++passengerId;
+    this.name = name;
+    store.passengers.push(this);
+  }
+
+  trips(){
+    return store.trips.filter(
+      function(trip){
+        return trip.passengerId === this.id;
+    }.bind(this)
+  );
+}
+
+  drivers(){
+    return this.trips().map(
+      function(trip){
+        return trip.driver();
+      }.bind(this)
+    );
+  }
+}
+
+let tripId = 0
+
+class Trip {
+  constructor(driver, passenger){
+    this.id = ++tripId;
+    if (driver) {
+      this.driverId = driver.id;
+    }
+    if (passenger) {
+      this.passengerId = passenger.id;
+    }
+    store.trips.push(this);
+  }
+
+  passenger(){
+    return store.passengers.find(
+      function(passenger) {
+        return passenger.id === this.passengerId;
+      }.bind(this)
+    );
+  }
+  driver(){
+    return store.drivers.find(
+      function(driver) {
+        return driver.id === this.driverId;
+      }.bind(this)
+    );
+  }
+}
+>>>>>>> c1ea1ce0a828e05ff07c5f3776d6ae99e4ea346a
 }
